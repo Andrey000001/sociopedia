13d084b2bfd4bda8a332241616e8f1b0edfdbbd3
Andrey <kovratovskiy1999@gmail.com>
2025-05-13 12:07:44 -0400

    created widgets,some scenes ,a couple of components ,fix a bit problem with back end

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,13 @@
+import {
+  ChatBubbleOutlined,
+  FavoriteBorderOutlines,
+  FavoriteOutlined,
+  ShareOutlined,
+} from '@mui/icons-material';
+import { Box, Divider, IconButton, Typography, useTheme } from '@mui/material';
+import FlexBetween from 'components/FlexBetween';
+import Friend from 'component/Friend';
+const PostWidget = () => {
+  return <></>;
+};
+export default PostWidget;
